05848849a671c33de09b8b1445be205ddd34fa51
Henry Ye <cse498@cse498s-iMac-43.local>
2016-09-21 10:48:13 -0400

    with selection

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,9 +1,15 @@
 
 import os
 import django.contrib.auth
+import dj_database_url
+
+
+
 django.contrib.auth.LOGIN_URL = '/'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 SECRET_KEY = 'usty1l$zk$w^+-6uxuk)h4un3we2ko5+(azcr)v_lki(b63w8h'
 
@@ -94,7 +100,18 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
+    os.path.join(STATIC_ROOT, 'static'),
+)
+STATICFILES_DIRS = (
     ('css', os.path.join(STATIC_ROOT , 'css')),
     ('js', os.path.join(STATIC_ROOT , 'js')),
     ('images', os.path.join(STATIC_ROOT , 'images')),
 )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
